b50f6ea83dc39b615db1f164b35e618d3262cea9
John-Dev <tst.1a2a3@gmail.com>
2024-05-20 23:52:59 +0200

    Add User and Car repository interface

---
+++ src/main/java/com/rent/rentacar/repositories/CarRepository.java
@@ -0,0 +1,7 @@
+package com.rent.rentacar.repositories;
+
+import com.rent.rentacar.models.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarRepository extends JpaRepository<Car, Integer> {
+}
