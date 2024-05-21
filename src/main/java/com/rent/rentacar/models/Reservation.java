997cfbcdd803403a92fdbec51472d20a52e61657
John-Dev <tst.1a2a3@gmail.com>
2024-05-21 19:35:50 +0200

    Add Reservation model and repository

---
+++ src/main/java/com/rent/rentacar/models/Reservation.java
@@ -0,0 +1,40 @@
+package com.rent.rentacar.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "reservation")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "reservation_id")
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "car_id", nullable = false)
+    private Car car;
+
+    @Column(nullable = false)
+    @JsonProperty("start_at")
+    private LocalDateTime startAt;
+
+    @Column(nullable = false)
+    @JsonProperty("end_at")
+    private LocalDateTime endAt;
+
+    @Column(nullable = false)
+    private double total_price;
+}
---
+++ src/main/java/com/rent/rentacar/repositories/ReservationRepository.java
@@ -0,0 +1,9 @@
+package com.rent.rentacar.repositories;
+
+import com.rent.rentacar.models.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
+}
